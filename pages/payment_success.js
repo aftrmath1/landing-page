62354d0beeec5ee886d3c606f674fb68dc9cf6a5
lmaosoggypancakes <macerajosef@gmail.com>
2023-08-20 01:44:34 -0700

    fix: build errors

--- pages/payment_success.js
+++ pages/payment_success.js
@@ -2,5 +2,5 @@ import { useRouter } from "next/router";
 
 export default function Home() {
   const { query } = useRouter();
-  return <main className="bg-primary">Payment Success! {query}</main>;
+  return <main className="bg-primary">Payment Success!</main>;
 }
