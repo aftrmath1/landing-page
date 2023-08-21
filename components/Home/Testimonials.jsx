fee81f76096e8dd212a0016df5577164d31c51ec
prayagtam <tamarisakandalap@gmail.com>
2023-08-21 12:55:31 -0400

    fixed some padding

--- components/Home/Testimonials.jsx
+++ components/Home/Testimonials.jsx
@@ -1,6 +1,6 @@
 export default function Testimonials() {
   return (
-    <section className="mx-auto max-w-7xl px-5 md:px-0">
+    <section className="mx-auto max-w-7xl px-5 md:px-10">
       <div className="">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl mb-10">
           Hear from our customers.
