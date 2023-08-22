6c4695fee0fcf5acb14a6f97323535bfc971afe4
prayagtam <tamarisakandalap@gmail.com>
2023-08-22 18:09:11 -0400

    ypo

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,80 +1,29 @@
-import React, { useState } from 'react';
-import firebase from '../lib/firebaseClient';
-import firestore from 'firebase/firestore';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 export default function ContactForm() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const form = useRef();
 
-  const handleSubmit = async (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
 
-    try {
-      await firebase.firestore().collection('contacts').add({
-        name,
-        email,
-        message,
-        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    emailjs.sendForm //put the ids here/')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
       });
-
-      // Clear form fields
-      setName('');
-      setEmail('');
-      setMessage('');
-    } catch (error) {
-      console.error('Error saving contact:', error);
-    }
   };
 
   return (
-    <div className="relative isolate md:-translate-y-10 lg:-translate-y-12 bg-white z-10 sm:mx-10 md:mx-20 lg:mx-32 md:rounded-xl rounded-none shadow-xl">
-      <div className="px-6 py-8 text-black">
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="name" className="text-xl mt-6 font-semibold">
-            Name
-          </label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="w-full px-4 py-2 mt-2 border rounded-md"
-            required
-          />
-
-          <label htmlFor="email" className="text-xl mt-6 font-semibold">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="w-full px-4 py-2 mt-2 border rounded-md"
-            required
-          />
-
-          <label htmlFor="message" className="text-xl mt-6 font-semibold">
-            Message
-          </label>
-          <textarea
-            id="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            className="w-full px-4 py-2 mt-2 border rounded-md"
-            rows="4"
-            required
-          />
-
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 mt-6 rounded-md"
-          >
-            Submit
-          </button>
-        </form>
-      </div>
-    </div>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="from_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
   );
-}
+};
