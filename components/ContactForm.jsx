92780b9a58ba205b102a643889cb46503b5aa86a
prayagtam <tamarisakandalap@gmail.com>
2023-08-22 18:40:17 -0400

    pushed styling

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -7,7 +7,7 @@ export default function ContactForm() {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm //put the ids here/')
+  emailjs.sendForm ({/*fill ids in*/})
       .then((result) => {
           console.log(result.text);
       }, (error) => {
@@ -16,14 +16,20 @@ export default function ContactForm() {
   };
 
   return (
+    <div className="relative isolate md:-translate-y-10 lg:-translate-y-12 bg-white z-10 sm:mx-10 md:mx-20 lg:mx-32 md:rounded-xl rounded-none shadow-xl">
+    <div className="px-6 py-8 text-black">
     <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="from_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
+      <label htmlFor="name" className="text-xl mt-6 font-semibold">Name</label>
+      <input type="text" name="from_name" className="w-full px-4 py-2 mt-2 border rounded-md" />
+      <label className="text-xl mt-6 font-semibold" >Email</label>
+      <input type="email" name="user_email"  className="w-full px-4 py-2 mt-2 border rounded-md" />
+      <label className="text-xl mt-6 font-semibold" >Message</label>
+      <textarea name="message" className="w-full px-4 py-2 mt-2 border rounded-md"
+            rows="4"
+            required />
+      <input type="submit" value="Send"  />
     </form>
+    </div>
+    </div>
   );
 };
